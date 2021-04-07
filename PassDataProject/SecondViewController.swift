0e0d5d6bf50b57449df2db77c8644780dd0b9883
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-07 23:07:00 +0300

    Data transfer to second screen was implemented.

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var login: String?
     @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let realLogin = login else {return}
+        label.text = "Hello, \(realLogin)"
+    }
     
     @IBAction func goBackPressed(_ sender: UIButton) {
     }
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func loginPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSeque", sender: nil)
     }
     
     // Hide keyboard by touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc =  segue.destination as? SecondViewController else {return}
+        dvc.login = loginTextField.text
     }
 }
 
